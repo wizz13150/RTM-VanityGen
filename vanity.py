9b59b3f198241b221cd848ee2c1214df18d76dbc
Wizz_^ <lewizard@hotmail.fr>
2022-12-21 03:17:00 +0100

    Update vanity.py

--- vanity.py
+++ vanity.py
@@ -52,6 +52,10 @@ def find_it(search_for: list, start: bool):
     found = 0
     address_count = 0
     start_time = time.time()
+    for char in options.string:
+        if char not in alphabet:
+            print(f"Character '{char}' not allowed, see : '{alphabet}'.")
+            return
     while True:
         pk = urandom(32).hex()
         key = Key(pk)
