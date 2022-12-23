a2e11749f25b919e1d13070386476ce8dea29c36
Wizz_^ <lewizard@hotmail.fr>
2022-12-23 23:56:02 +0100

    Update vanity_upper.py

--- vanity_upper.py
+++ vanity_upper.py
@@ -1,10 +1,11 @@
 """
-Test - Generate vanity ecdsa address for Raptoreum - v2
+Test - Generate vanity ecdsa address for Raptoreum
 
 Affero GPL
 You can use and modify freely, including in paid services and saas,
 as long as you release the source code and modifications.
 """
+
 
 from os import urandom
 from time import time
@@ -16,18 +17,17 @@ from coincurve import PrivateKey
 import base58
 from tornado.options import define, options
 import subprocess
-import signal
-import sys
 
 
 define("processes", default=4, help="Process count to start (default 4)", type=int)
+define("string", help="String to find in the address", type=str)
+define("start", default=False, help="search for string at the start of the address (default false)", type=bool)
 define("max", default=100, help="max hit per process (default 100)", type=int)
 
 
-alphabet = '|123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz|'
+alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 file = "./keys_upper.txt"
 NETWORK_PREFIX = 60
-string = "R"
 
 
 class Key:
@@ -47,55 +47,80 @@ class Key:
         return base58.b58encode(vh160 + chk).decode('utf-8')
 
 
-def find_it(string: str):
+def find_it(search_for: list, start: bool):
     found = 0
     address_count = 0
     address_total = 0
     start_time = time.time()
     timer = time.perf_counter()
     with open(file, "a") as f:
-        while found < options.max:
+        while True:
             pk = urandom(32).hex()
             key = Key(pk)
             address = key.address
             address_count += 1
             address_total += 1
             if address.isupper() == True:
-                command = ["./ravencoin-tool/bitcoin-tool", "--input-type", "private-key", "--input-format", "hex", "--public-key-compression", "compressed", "--input", pk, "--network", "ravencoin", "--output-type", "private-key-wif", "--output-format", "base58check"]
-                result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                if result.returncode == 0:
-                    wif = result.stdout.decode().strip()
-                    print("\n" + "-" * 20)
-                    print(f"Address : {key.address}")
-                    print(f"HEX     : {pk}")
-                    print(f"WIF     : {wif}")
-                    f.write("\n" + "-" * 20)
-                    f.write(f"\nAddress : {key.address}")
-                    f.write(f"\nHEX     : {pk}")
-                    f.write(f"\nWIF     : {wif}")
-                    found += 1
+                if options.start:
+                    for string in search_for:
+                        if address.startswith(string):
+                            found += 1
+                            command = ["./ravencoin-tool/bitcoin-tool", "--input-type", "private-key", "--input-format", "hex", "--public-key-compression", "compressed", "--input", pk, "--network", "ravencoin", "--output-type", "private-key-wif", "--output-format", "base58check"]
+                            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            if result.returncode == 0:
+                                wif = result.stdout.decode().strip()
+                                print("\n" + "-" * 20)
+                                print(f"\nAddress : {key.address}")
+                                print(f"HEX     : {pk}")
+                                print(f"WIF     : {wif}")
+                                f.write("\n" + "-" * 20)
+                                f.write(f"\nAddress : {key.address}")
+                                f.write(f"\nHEX     : {pk}")
+                                f.write(f"\nWIF     : {wif}")                        
+                                if found > options.max:
+                                    return
+                else:
+                    for string in search_for:
+                        if string in address:
+                            found += 1
+                            command = ["./ravencoin-tool/bitcoin-tool", "--input-type", "private-key", "--input-format", "hex", "--public-key-compression", "compressed", "--input", pk, "--network", "ravencoin", "--output-type", "private-key-wif", "--output-format", "base58check"]
+                            result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            if result.returncode == 0:
+                                wif = result.stdout.decode().strip()
+                                print("\n" + "-" * 20)
+                                print(f"\nAddress : {key.address}")
+                                print(f"HEX     : {pk}")
+                                print(f"WIF     : {wif}")
+                                f.write("\n" + "-" * 20)
+                                f.write(f"\nAddress : {key.address}")
+                                f.write(f"\nHEX     : {pk}")
+                                f.write(f"\nWIF     : {wif}")
+                                if found > options.max:
+                                    return
             current_time = time.time()
-            if current_time - start_time >= 1:
-                elapsed_time = time.perf_counter() - timer
-                minutes, seconds = divmod(elapsed_time, 60)
-                print(f"\r{address_count * options.processes} addresses generated per second... Total: {address_total * options.processes / 1_000_000:.2f}m in {minutes:.0f}min {seconds:.0f}sec", end="")
+            if current_time - start_time > 1:
+                print(f"\r{address_count * options.processes} addresses generated per second... Total: {address_total * options.processes / 1_000_000:.2f}m in {time.perf_counter() - timer:.0f} seconds", end="")
                 address_count = 0
                 start_time = current_time
 
-def exit_gracefully(signum, frame):
-    sys.exit(0)
-signal.signal(signal.SIGINT, exit_gracefully)
-
 def main():
     options.parse_command_line()
-    print("")
     print("Looking for UPPERCASE")
-    print(f"Output logged in {file}")
+    print(f"Output in {file}")
     print(f"{options.processes} threads used")
+    if options.start:
+        print("Search at beginning of address")
+    else:
+        print("Search anywhere in the address")
+
     processes = []
+    if "|" in options.string:
+        search_for = options.string.split("|")
+    else:
+        search_for = [options.string]
     with ProcessPoolExecutor(max_workers=options.processes) as executor:
         for i in range(options.processes):
-            executor.submit(find_it, string)
+            executor.submit(find_it, search_for, options.start)
     for p in processes:
         p.join()
 
